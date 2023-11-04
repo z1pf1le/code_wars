a0cb3ef14a6d42c3de4919cb3a87492a744d674f
andrei <ssignal.fills.the.void@gmail.com>
2023-11-04 18:49:34 +0300

    sb

--- src/Troll.java
+++ src/Troll.java
@@ -1,11 +1,14 @@
+import java.util.regex.Pattern;
+
 public class Troll {
     public static String disemvowel(String str) {
         StringBuffer sb = new StringBuffer();
         int count = 0;
-
+        String VOWELS = "aeiouAEIOU";
         while(count < str.length()){
-            sb.append(str.charAt(count));
-
+            if (VOWELS.indexOf(str.charAt(count)) == -1) {
+                sb.append(str.charAt(count));
+            }
             count ++;
             }
 

aca99bae6fd757dec3455bc394da5fc99958f2c0
z1pf1le <total.zip.file@gmail.com>
2023-11-04 14:04:47 +0300

    Initial commit

---
+++ src/Troll.java
@@ -0,0 +1,20 @@
+public class Troll {
+    public static String disemvowel(String str) {
+        // Code away...
+
+//        System.out.println("fag");
+//        return str;
+
+        //new Integer = 1;
+//        public static void sayWord()
+//    {
+//
+//    }
+
+        System.out.println(str);
+            return str;
+
+
+
+    }
+}
